dc0728095d351afd4e751f64b361aea42bb1a592
Arthur Sh <arthur.shemshur@gmail.com>
2024-01-31 09:24:09 +0200

    Initial Commit

---
+++ HW-13-settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-13-settings
+//
+//  Created by Arthur Sh on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
